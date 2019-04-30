03de30e2fbd43d2db234e2b3c1df91e30b208a5a
Alex Harvey <alexharv074@gmail.com>
2019-05-01 01:48:59 +1000

    Add some docs and tests

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cfn-init Rspec validation
 
+## Overview
+
+A proof of concept using Rspec to validate AWS::CloudFormation::Init configurations.
+
+## Usage
+
+This is a demo. To run it:
+
+```text
+▶ bash test_runner.sh
+```
+
+The test runner downloads a bunch of example CloudFormation templates with AWS::CloudFormation::Init blocks and validates them.
--- test_runner.sh
+++ test_runner.sh
@@ -9,10 +9,29 @@ if ! which -s cfn-flip ; then
   . venv/bin/activate
 fi
 
+echo "This is a demo of Rspec validation of sample AWS CloudFormation templates."
+echo "See spec/validate_spec.rb for more info."
+sleep 2
+
+set -x
+
+# JSON templates.
 for i in \
-  https://gist.githubusercontent.com/miyamoto-daisuke/6087331/raw/807352ff593327089689a1e6f71fb469d0f8ae11/cfn-init.template
+  https://gist.githubusercontent.com/miyamoto-daisuke/6087331/raw/807352ff593327089689a1e6f71fb469d0f8ae11/cfn-init.template \
+  https://raw.githubusercontent.com/stackmate/stackmate/master/templates/AWS/LAMP_Single_Instance.template
 do
   curl -s "$i" -o cloudformation.json
   cfn-flip -y cloudformation.json > cloudformation.yml
-  bundle exec rspec spec/validate_spec.rb
+  bundle exec rspec spec/validate_spec.rb || exit $?
 done
+
+# YAML templates.
+for i in \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/windows.template \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/centos.template
+do
+  curl -s "$i" -o cloudformation.yml
+  bundle exec rspec spec/validate_spec.rb || exit $?
+done
+
+echo "All tests passed."
