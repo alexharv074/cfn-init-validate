c0c08c39365a536f73d7dd261f3a45e72fdac8f6
Alex Harvey <alexharv074@gmail.com>
2019-05-01 14:46:32 +1000

    Add a lot more example templates for testing

--- test_runner.sh
+++ test_runner.sh
@@ -19,7 +19,13 @@ set -x
 # JSON templates.
 for i in \
   https://gist.githubusercontent.com/miyamoto-daisuke/6087331/raw/807352ff593327089689a1e6f71fb469d0f8ae11/cfn-init.template \
-  https://raw.githubusercontent.com/stackmate/stackmate/master/templates/AWS/LAMP_Single_Instance.template
+  https://raw.githubusercontent.com/stackmate/stackmate/master/templates/AWS/LAMP_Single_Instance.template \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/HelperNonAmaznAmi/RHEL7_cfn-hup.template \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/services/CloudFormation/HostnameChangeRHEL-Metadata.template \
+  https://s3.amazonaws.com/cloudformation-templates-us-east-1/Drupal_Single_Instance.template \
+  https://raw.githubusercontent.com/aws-samples/aws-cfn-custom-resource-examples/master/examples/dns-mapping/example.template \
+  https://raw.githubusercontent.com/rowlinsonmike/cf_win_template/master/cf_win_template.json \
+  https://s3.amazonaws.com/cloudformation-templates-us-east-1/Windows_Single_Server_SharePoint_Foundation.template
 do
   curl -s "$i" -o cloudformation.json
   cfn-flip -y cloudformation.json > cloudformation.yml
@@ -29,11 +35,17 @@ done
 # YAML templates.
 for i in \
   https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/windows.template \
-  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/centos.template
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/centos.template \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/services/AutoScaling/AutoScalingRollingUpdates.yaml \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/solutions/AmazonCloudWatchAgent/inline/ubuntu.template \
+  https://raw.githubusercontent.com/awslabs/aws-cloudformation-templates/master/aws/services/ElasticLoadBalancing/ELB_Access_Logs_And_Connection_Draining.yaml
 do
   curl -s "$i" -o cloudformation.yml
   bundle exec rspec spec/validate_spec.rb || exit $?
 done
+
+# Unsupported: If condition in Boolean not being handled:
+#   https://raw.githubusercontent.com/widdix/aws-cf-templates/master/ec2/ec2-auto-recovery.yaml
 
 set +x
 
